43cd82bbd1ef669d469940ed11b5d168c54705f7
Anton Kulikov <tonyk@mail.ru>
2024-10-07 17:01:14 +0300

    add modules 2

---
+++ messenger.practicum/src/modules/chats/components/profile-bar/profile-bar.js
@@ -0,0 +1,18 @@
+let profileBar =  function(){
+    return `<div class="profile-bar">
+                
+                <div class="profile-bar__button>
+                <div class="profile-bar__label>Профиль</div>
+                    <div class="profile-bar__icon>
+                        <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 7L15 12L10 17" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                
+                </div>
+            </div>
+  `
+};
+
+
+export {profileBar};
